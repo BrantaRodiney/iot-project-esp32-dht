5c2807a073b6d8475be85bdd53c20baf64ebe2da
Rodiney Branta <branta.rodiney@gmail.com>
2021-09-26 01:31:18 -0300

    optimized both functions to receive and post

--- thingspeak_lib.py
+++ thingspeak_lib.py
@@ -7,18 +7,12 @@ THINGSPEAK_APT_WRITE_KEY = ''
 THINGSPEAK_API_READ_KEY = ''
 HTTP_HEADER= {'Content-Type': 'application/json'}
 
-def send_data():
+def receive_sensor_data_and_post_it():
     temperature, humidity = dht_sensor.get_sensor_data()
-    request = urequests.post(
-        'http://api.thingspeak.com/update?api_key=' +
-        THINGSPEAK_APT_WRITE_KEY,
-        json = {'field1':temperature,'field2':humidity},
-        headers = HTTP_HEADER
-    )
-    request.close()
+    send_data(temperature,humidity)
     print(temperature,humidity)
     
-def send_direct_data(temperature, humidity):
+def send_data(temperature, humidity):
     request = urequests.post(
         'http://api.thingspeak.com/update?api_key=' +
         THINGSPEAK_APT_WRITE_KEY,
@@ -26,4 +20,3 @@ def send_direct_data(temperature, humidity):
         headers = HTTP_HEADER
     )
     request.close()
-    
